a698f8d37071d6502a21e9f04ee445b018ce9859
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-04-04 18:13:31 +0900

    Update Class_0404_Quiz1_P_main.java

--- 학생 30명의 성적데이터 과목별 총점수,평균 구하기/Class_0404_Quiz1_P_main.java
+++ 학생 30명의 성적데이터 과목별 총점수,평균 구하기/Class_0404_Quiz1_P_main.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 public class Class_0404_Quiz1_P_main {
 	public static void main(String[] args) {
 
-		//학생 배열만들기 + 클래스를 받기
+		//학생 배열만들기 + 클래스를 받아서 배열에 객체를 담는다.
 		Class_0404_Quiz1_P [] student = new Class_0404_Quiz1_P[30];
 		
 		
 		// 데이터 입력 
 		for (int i = 0; i < student.length; i++) {
 			//객체 생성하기
-			student[i] = new Class_0404_Quiz1_P();
+			student[i] = new Class_0404_Quiz1_P(); //배열 i마다 객체를 담는다.
 			// 데이터 입력
 			student[i].inputData();
 		}
 	
 	
 		// 계산
-		int korTotal = 0, engTotal = 0, mathTotal = 0;
+		int korTotal = 0, engTotal = 0, mathTotal = 0; //각 과목의 총 점수
 		for(int i = 0; i<student.length; i++) {
-			korTotal = korTotal + student[i].kor;
+			//각 i번째 학생마다의 점수들을 축적한다.
+			korTotal = korTotal + student[i].kor; 
 			engTotal = engTotal + student[i].eng;
 			mathTotal= mathTotal + student[i].math;
 		}
